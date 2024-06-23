066710c8899ded86070ff1daa7a5b2709d051bf4
Nicholas Sullivan <nicksullivan136@gmail.com>
2024-06-22 19:46:17 -0500

    Added functionality for most frequent committers. Added perl file for processing and associated call to main script.

--- gitstats.sh
+++ gitstats.sh
@@ -3,14 +3,21 @@
 
 #TODO STATS
 # Average Commit Size       
-# Most common committer
 # First commit date
-# Programming Languages used/detected
+# Programming Languages used/detected (use github-linguist)
+# Options/Shell Parameters
+
+#FIXME 
+# Arguments too long for xargs 
+# Following symlinks create infinite loop (dont follow symbolic links)
+
+# Absolute file path to our directory
+BASEDIR="$(dirname $(realpath -s $0))" #  Credit: https://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
 
 # Find all Git projects on a system
 function getGit {
-    find . -name .git -type d -prune
-    # find /home -name .git -type d -prune
+    # find ../ -name .git -type d -prune
+    find /home -name .git -type d -prune
 }
 
 # Read all .git project files on system into array variable
@@ -23,14 +30,17 @@ for x in "${gitfiles[@]}";
 do
     # Returns size of local repository
     x="$(dirname ${x})"
-    du -sh $x
+    echo ""
+    du -sh $x &
+    wait $! # Waits until last command finishes
+    
 
-    # Most common committers
-    # Get all commits with PCRE grep and put them in an array (easier way to do this?)
-    mapfile gitcommits < <( git log | grep -Pz 'commit.*(?!commit)')
+    # Most frequent committers
+    # Will require perl script to create a hash variable
+    # Create a subshell in the next directory, grep all commits from the git log, and pass them to the perl script for processing
+    (cd $x && git log | grep -Pz 'commit.*(?!commit)' | xargs -0 perl $BASEDIR/commits.pl)
+    
+
 done
-
-
-# git log --stat
 
 
